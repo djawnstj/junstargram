f973e42ee6c27ad76f52d9bc99fbfb71ccb4311a
junseo <djawnstj@naver.com>
2022-09-12 19:36:40 +0900

    Feat: 폼, DTO 생성
    로그인폼, 회원DTO 파일 추가

---
+++ src/main/kotlin/com/junstargram/dto/MemberDTO.kt
@@ -0,0 +1,11 @@
+package com.junstargram.dto
+
+class MemberDTO {
+
+    var uid: Long = -1
+
+    lateinit var memId: String
+    lateinit var memPw: String
+    lateinit var memImgPath: String
+
+}
---
+++ src/main/kotlin/com/junstargram/form/LoginForm.kt
@@ -0,0 +1,10 @@
+package com.junstargram.form
+
+import javax.validation.constraints.NotEmpty
+
+class LoginForm {
+    @NotEmpty(message = "ID를 입력하지 않았습니다.")
+    lateinit var memId: String
+    @NotEmpty(message = "PW를 입력하지 않았습니다.")
+    lateinit var 가memPw: String
+}
